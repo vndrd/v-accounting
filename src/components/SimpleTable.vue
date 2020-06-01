b5f1935f29b1c185c9826584d7cbcb33b38293f8
vndrd <ivangddzzz@gmail.com>
2020-05-31 23:47:52 -0400

    estilos

--- src/components/SimpleTable.vue
+++ src/components/SimpleTable.vue
@@ -1,36 +1,39 @@
 <template>
-<b-table-simple responsive>
+<b-table-simple responsive striped hover>
   <b-thead>
     <b-tr>
-      <b-th>Cuenta</b-th>
-      <b-th>Debe</b-th>
-      <b-th>Haber</b-th>
-      <b-th></b-th>
+      <b-th style="width: 15rem">CUENTA</b-th>
+      <b-th>DEBE</b-th>
+      <b-th>HABER</b-th>
+      <b-th style="width: 1rem"></b-th>
     </b-tr>
   </b-thead>
   <b-tbody>
+    
     <b-tr v-for="cuenta in getAsientos" :key="cuenta.id">
       <b-td>{{cuenta.nombre}}</b-td>
       <b-td>{{valueOrZero(cuenta,'Debe')}}</b-td>
       <b-td>{{valueOrZero(cuenta,'Haber')}}</b-td>
-      <b-th>
+      <b-td style="width: 1rem">
             <b-button size="sm" 
             @click="quitarAsiento(cuenta.id)" 
             class="mr-1" 
             v-if="cuenta.id!=1000">
                 <b-icon-trash-fill />
             </b-button>
-      </b-th>
+      </b-td>
     </b-tr>
   </b-tbody>
-  <b-tfoot>
+  <b-tfoot  class="mt-5">  
     <b-tr>
-        <b-td>Total</b-td>
-        <b-td>{{sumarDebe}}</b-td>
-        <b-td>{{sumarHaber}}</b-td>
+        <b-td>
+            <span class="float-right bold">Total:</span>
+        </b-td>
+        <b-td>{{sumarDebe}}.-</b-td>
+        <b-td>{{sumarHaber}}.-</b-td>
         <b-td>
             <b-button variant="success" :disabled="sumarHaber!==sumarDebe">
-                Agregar
+                Registrar
             </b-button>
         </b-td>
     </b-tr>
@@ -51,7 +54,7 @@ export default {
         ...mapActions(['quitarAsiento']),
         valueOrZero(cuenta,tipo){
             if ( cuenta.tipo === tipo  ) 
-                return cuenta.monto
+                return `${cuenta.monto}.-`
             return '';
 
         },
